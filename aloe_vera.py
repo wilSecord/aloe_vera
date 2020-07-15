e85d130d7b894fd42bfd082428492fd5f8c4b3dd
Wil <wilsonsecord@gmail.com>
2020-07-14 22:53:33 -0400

    Movement currently in the works

--- aloe_vera.py
+++ aloe_vera.py
@@ -3,6 +3,8 @@ import random
 # Defining spawn location
 sly = random.randint(0, 9)
 slx = random.randint(0, 9)
+# Defining what the player spawns on
+o = '.'
 
 # Defining the random game board
 ar = [
@@ -42,6 +44,14 @@ fi = [
     ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
 ]
 
+
+# Drawing game board
+def draw(gb):
+    for thing in gb:
+        st = ' '.join(thing)
+        print(st)
+
+
 # Replacing any random float > 0.7 with '+'
 for item in ar:
     for val in item:
@@ -50,11 +60,71 @@ for item in ar:
             e = item.index(val)
             fi[i][e] = '+'
 fi[slx][sly] = '#'
-
-
-# Drawing game board
-def draw(gb):
-    for item in gb:
-        st = ' '.join(item)
-        print(st)
-draw(fi)
+print('''
+''')
+while True:
+    mo = input('> ')
+    if mo == 's':
+        for item in fi:
+            if '#' in item:
+                t = fi.index(item)
+                t2 = item.index('#')
+                o2 = fi[t + 1][t2]
+                if fi[t + 1][t2] == 1:
+                    break
+                else:
+                    fi[t + 1][t2] = '#'
+                    fi[t][t2] = o
+                    o = o2
+                    break
+        draw(fi)
+        print('''
+        ''')
+    elif mo == 'w':
+        for item in fi:
+            if '#' in item:
+                t = fi.index(item)
+                t2 = item.index('#')
+                o2 = fi[t - 1][t2]
+                if fi[t +- 1][t2] == 1:
+                    break
+                else:
+                    fi[t - 1][t2] = '#'
+                    fi[t][t2] = o
+                    o = o2
+                    break
+        draw(fi)
+        print('''
+        ''')
+    elif mo == 'd':
+        for item in fi:
+            if '#' in item:
+                t = fi.index(item)
+                t2 = item.index('#')
+                o2 = fi[t][t2 + 1]
+                if fi[t][t2 + 1] == 1:
+                    break
+                else:
+                    fi[t][t2 + 1] = '#'
+                    fi[t][t2] = o
+                    o = o2
+                    break
+        draw(fi)
+        print('''
+        ''')
+    elif mo == 'a':
+        for item in fi:
+            if '#' in item:
+                t = fi.index(item)
+                t2 = item.index('#')
+                o2 = fi[t][t2 - 1]
+                if fi[t][t2 - 1] == 1:
+                    break
+                else:
+                    fi[t][t2 - 1] = '#'
+                    fi[t][t2] = o
+                    o = o2
+                    break
+        draw(fi)
+        print('''
+        ''')
